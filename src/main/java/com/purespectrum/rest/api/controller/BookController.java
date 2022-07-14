563decd4583a1629c27462a7976176308b717bd5
Airton Junior <airton.manfrini@hotmail.com>
2022-07-14 19:16:43 -0300

    fix: book controller

--- src/main/java/com/purespectrum/rest/api/controller/BookController.java
+++ src/main/java/com/purespectrum/rest/api/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookController {
 
@@ -21,6 +24,17 @@ public class BookController {
     public ResponseEntity getBook(@PathVariable("bookCode") Long code){
         return repository.findById(code).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build());
     }
+
+    @GetMapping(path = "/api/book/author/{authorName}")
+    public ResponseEntity getBookByAuthor(@PathVariable("authorName") String author){
+        List<BookModel> result = new ArrayList<BookModel>();
+        repository.findAll().forEach(book -> {result.add(book);});
+        return ResponseEntity.ok().body((result));
+    }
+
+
+
+    @GetMapping(path = "/api/book/")
 
     @PostMapping(path = "/api/book/new")
     public BookModel save(@RequestBody BookModel book){
--- src/main/java/com/purespectrum/rest/api/repository/BookRepository.java
+++ src/main/java/com/purespectrum/rest/api/repository/BookRepository.java
@@ -3,8 +3,7 @@ package com.purespectrum.rest.api.repository;
 import com.purespectrum.rest.api.model.BookModel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<BookModel, Long> {
-
-
-
 }
